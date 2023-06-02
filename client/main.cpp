e2010b59b76d4cfc4230bba273eea3443445757b
mac <mac@macs-MacBook-Pro.local>
2023-06-03 02:40:19 +0300

    Added security && Added temperature && Added show method

--- client/main.cpp
+++ client/main.cpp
@@ -4,7 +4,7 @@
 using boost::asio::ip::tcp;
 
 void handle_command(tcp::socket& socket, std::string command) {
-    std::array<char, 256> buffer;
+    std::array<char, 256>     buffer;
     boost::system::error_code error;
 
     boost::asio::write(socket, boost::asio::buffer(command));
@@ -14,7 +14,7 @@ void handle_command(tcp::socket& socket, std::string command) {
         std::cerr << "Error receiving response: " << error.message() << std::endl;
     } else {
         std::string response(buffer.data(), bytes_transferred);
-        std::cout << "Received response: " << response << std::endl;
+        std::cout << response << std::endl;
     }
 }
 
@@ -31,7 +31,8 @@ int main() {
               << "1. on/off light\n"
               << "2. on/off security\n"
               << "3. control temperature\n"
-              << "4. exit\n\n";
+              << "4. show conditions\n"
+              << "5. exit\n\n";
 
     bool isRunning = true;
     int  s{};
@@ -39,8 +40,8 @@ int main() {
     std::array<char, 256>     buffer;
     boost::system::error_code error;
 
-    size_t bytes_transferred = 0;
-    std::string commands[] = {"light", "security"};
+    size_t      bytes_transferred = 0;
+    std::string commands[]        = {"light", "security", "temperature", "show"};
     while (isRunning) {
         std::cout << "Choose an option: ";
         std::cin >> s;
@@ -50,7 +51,26 @@ int main() {
                 std::cout << "1";
                 break;
             case 2:
-                handle_command(socket, commands[0]);
+                handle_command(socket, commands[1]);
+                break;
+            case 3:
+                int num;
+                do {
+                    std::cout << "Enter the desired temperature (in degrees Celsius: [-10 : 40]) ";
+                    std::cin >> num;
+                    if (std::cin.fail()) {
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    }
+                } while (num < -10 || num > 40);
+                handle_command(socket, commands[2] + std::to_string(num));
+                break;
+            case 4:
+                handle_command(socket, commands[3]);
+                break;
+            case 5:
+                socket.close();
+                exit(0);
                 break;
             default:
                 std::cout << "Invalid option.\n";
--- server/main.cpp
+++ server/main.cpp
@@ -12,18 +12,9 @@ class SecurityDatabase
         int rc = sqlite3_open(db_name.c_str(), &m_db);
         if (rc != SQLITE_OK) {
             std::cerr << "Error opening database: " << sqlite3_errmsg(m_db) << std::endl;
-            sqlite3_close(m_db);
-            m_db = nullptr;
-        } else {
-            // создаем таблицу 'security', если она не существует
-            std::string create_query("CREATE TABLE IF NOT EXISTS security (name TEXT, condition INTEGER);");
-            rc = sqlite3_exec(m_db, create_query.c_str(), nullptr, nullptr, nullptr);
-            if (rc != SQLITE_OK) {
-                std::cerr << "Error creating table: " << sqlite3_errmsg(m_db) << std::endl;
-                sqlite3_close(m_db);
-                m_db = nullptr;
-            }
+            return;
         }
+        create();
     }
     ~SecurityDatabase() {
         if (m_db) {
@@ -33,7 +24,7 @@ class SecurityDatabase
     }
 
     bool update_security_condition(bool value) {
-        const char* condition    = value ? "1" : "0"; // заменили "true" и "false" на 1 и 0 соответственно
+        const char* condition    = value ? "1" : "0";
         char*       update_query = sqlite3_mprintf("UPDATE security SET condition = '%q' WHERE name = 'security'", condition);
         int         rc           = sqlite3_exec(m_db, update_query, nullptr, nullptr, nullptr);
         sqlite3_free(update_query);
@@ -44,16 +35,149 @@ class SecurityDatabase
         return m_db != nullptr;
     }
 
+    bool is_security_enabled() {
+        std::string query("SELECT condition FROM security WHERE name='security';");
+        int         cond = 0;
+        int         rc   = sqlite3_exec(
+            m_db, query.c_str(),
+            [](void* data, int argc, char** argv, char** az_col_name) -> int {
+                int* cond = static_cast<int*>(data);
+                *cond     = std::atoi(argv[0]);
+                return 0;
+            },
+            &cond, nullptr);
+        return cond != 0;
+    }
+
   private:
     std::string m_name;
     sqlite3*    m_db = nullptr;
+    static int  callback_count(void* data, int argc, char** argv, char** /*azColName*/) {
+        int* count = static_cast<int*>(data);
+        *count     = std::atoi(argv[0]);
+        return 0;
+    }
+    void create() {
+        std::string create_query("CREATE TABLE IF NOT EXISTS security (name TEXT, condition BOOLEAN);");
+        int         rc = sqlite3_exec(m_db, create_query.c_str(), nullptr, nullptr, nullptr);
+        if (rc != SQLITE_OK) {
+            std::cerr << "Error creating table: " << sqlite3_errmsg(m_db) << std::endl;
+            return;
+        } else {
+            // Проверяем не существует ли запись с именем "security" в таблице "security"
+            std::string select_query("SELECT COUNT(*) FROM security WHERE name='security';");
+            int         count = 0;
+            rc                = sqlite3_exec(m_db, select_query.c_str(), callback_count, &count, nullptr);
+            if (rc != SQLITE_OK) {
+                std::cerr << "Error selecting record count: " << sqlite3_errmsg(m_db) << std::endl;
+                return;
+            } else {
+                if (count == 0) {
+                    // Если записи с именем "security" нет, то добавляем ее в таблицу
+                    std::string insert_query("INSERT INTO security (name, condition) VALUES ('security', false);");
+                    rc = sqlite3_exec(m_db, insert_query.c_str(), nullptr, nullptr, nullptr);
+                    if (rc != SQLITE_OK) {
+                        std::cerr << "Error inserting record: " << sqlite3_errmsg(m_db) << std::endl;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 };
 
+class TemperatureDatabase
+{
+  public:
+    TemperatureDatabase(const std::string& db_name) : m_name(db_name) {
+        int rc = sqlite3_open(db_name.c_str(), &m_db);
+        if (rc != SQLITE_OK) {
+            std::cerr << "Error opening database: " << sqlite3_errmsg(m_db) << std::endl;
+            return;
+        }
+        create();
+    }
+    ~TemperatureDatabase() {
+        if (m_db) {
+            sqlite3_close(m_db);
+            m_db = nullptr;
+        }
+    }
+
+    bool update_security_condition(int value) {
+        char* update_query = sqlite3_mprintf("UPDATE temperature SET degrees = %d WHERE name = 'temperature'", value);
+        int   rc           = sqlite3_exec(m_db, update_query, nullptr, nullptr, nullptr);
+        sqlite3_free(update_query);
+        return rc == SQLITE_OK;
+    }
+
+    bool is_open() const {
+        return m_db != nullptr;
+    }
+
+    int get_temperature_from_database() {
+        std::string query("SELECT degrees FROM temperature WHERE name='temperature';");
+        int         temp = -1;
+
+        int rc = sqlite3_exec(
+            m_db, query.c_str(),
+            [](void* data, int argc, char** argv, char** az_col_name) -> int {
+                int* temp = static_cast<int*>(data);
+                *temp     = std::stoi(argv[0]);
+                return 0;
+            },
+            &temp, nullptr);
+
+        
+        if (rc != SQLITE_OK) {
+            std::cerr << "Error executing query: " << sqlite3_errmsg(m_db) << std::endl;
+        }
+
+        return temp;
+    }
+
+  private:
+    std::string m_name;
+    sqlite3*    m_db = nullptr;
+    static int  callback_count(void* data, int argc, char** argv, char** /*azColName*/) {
+        int* count = static_cast<int*>(data);
+        *count     = std::atoi(argv[0]);
+        return 0;
+    }
+    void create() {
+        std::string create_query("CREATE TABLE IF NOT EXISTS temperature (name TEXT, degrees INTEGER);");
+        int         rc = sqlite3_exec(m_db, create_query.c_str(), nullptr, nullptr, nullptr);
+        if (rc != SQLITE_OK) {
+            std::cerr << "Error creating table: " << sqlite3_errmsg(m_db) << std::endl;
+            return;
+        } else {
+            std::string select_query("SELECT COUNT(*) FROM temperature WHERE name='temperature';");
+            int         count = 0;
+            rc                = sqlite3_exec(m_db, select_query.c_str(), callback_count, &count, nullptr);
+            if (rc != SQLITE_OK) {
+                std::cerr << "Error selecting record count: " << sqlite3_errmsg(m_db) << std::endl;
+                return;
+            } else {
+                if (count == 0) {
+                    // Если записи с именем "temperature" нет, то добавляем ее в таблицу
+                    std::string insert_query("INSERT INTO temperature (degrees, name) VALUES (25, 'temperature');");
+                    rc = sqlite3_exec(m_db, insert_query.c_str(), nullptr, nullptr, nullptr);
+                    if (rc != SQLITE_OK) {
+                        std::cerr << "Error inserting record: " << sqlite3_errmsg(m_db) << std::endl;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+};
 
 void handle_connection(tcp::socket& socket) {
     std::array<char, 256>     buffer;
     boost::system::error_code error;
-    bool condition = false;
+
+    SecurityDatabase    s_db("security.db");
+    TemperatureDatabase t_db("temperature.db");
 
     // Keep reading from the socket until the client closes the connection
     while (true) {
@@ -72,29 +196,27 @@ void handle_connection(tcp::socket& socket) {
         std::cout << "Received message: " << message << std::endl;
 
         std::string response;
-        
-        if (message == "security") {
-            SecurityDatabase db("security.db");
-            if (db.is_open()) {
-                
 
-                if (db.update_security_condition(condition)) {
-                    if (condition)
-                        response = "Security condition updated successfully: [TRUE]";
-                    else
-                        response = "Security condition updated successfully: [FALSE]";
-                    condition = !condition;
+        if (message == "security") {
+            if (s_db.is_open()) {
+                if (s_db.update_security_condition(!s_db.is_security_enabled())) {
+                    response = "Security condition updated successfully: " + std::string(s_db.is_security_enabled() ? "on" : "off") + ".";
+
                 } else {
                     response = "Failed to update security condition\n";
                 }
             } else {
                 std::cout << "Failed to open database" << std::endl;
             }
-        } else if (message == "What is your name?") {
-            response = "My name is Server.";
-        } else if (message == "What time is it?") {
-            time_t now = std::time(nullptr);
-            response   = std::ctime(&now);
+        } else if (message.substr(0, 11) == "temperature") {
+            if (t_db.is_open()) {
+                std::cout << message.substr(11, 13);
+                t_db.update_security_condition(std::stoi(message.substr(11, 13)));
+                response = std::string("Temperature was succesfully changed to ") + std::to_string(t_db.get_temperature_from_database()) + ".";
+            }
+        } else if (message == "show") {
+            response = "Security system: " + std::string(s_db.is_security_enabled() ? "on" : "off") + ".\n" +
+                       "Temperature: " + std::to_string(t_db.get_temperature_from_database()) + ".";
         } else {
             response = "Unknown command.";
         }
