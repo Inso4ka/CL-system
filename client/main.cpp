6e6529e33ef1dcbf1fcec394f01ea39d2687cc70
mac <mac@macs-MacBook-Pro.local>
2023-06-02 17:06:32 +0300

    1-st test

--- client/main.cpp
+++ client/main.cpp
@@ -1,6 +1,62 @@
+#include <boost/asio.hpp>
 #include <iostream>
 
+using boost::asio::ip::tcp;
+
+void handle_command(tcp::socket& socket, std::string command) {
+    std::array<char, 256> buffer;
+    boost::system::error_code error;
+
+    boost::asio::write(socket, boost::asio::buffer(command));
+    size_t bytes_transferred = socket.read_some(boost::asio::buffer(buffer), error);
+
+    if (error) {
+        std::cerr << "Error receiving response: " << error.message() << std::endl;
+    } else {
+        std::string response(buffer.data(), bytes_transferred);
+        std::cout << "Received response: " << response << std::endl;
+    }
+}
+
 int main() {
-    std::cout << "Hello world!";
-    int i{};
+    boost::asio::io_service io_service;
+
+    tcp::resolver               resolver(io_service);
+    tcp::resolver::results_type endpoints = resolver.resolve("localhost", "1234");
+
+    tcp::socket socket(io_service);
+    boost::asio::connect(socket, endpoints);
+
+    std::cout << "Welcome to our smart home system. With our app, you can control the temperature, adjust the lighting, and keep your home safe and secure.\n\n"
+              << "1. on/off light\n"
+              << "2. on/off security\n"
+              << "3. control temperature\n"
+              << "4. exit\n\n";
+
+    bool isRunning = true;
+    int  s{};
+
+    std::array<char, 256>     buffer;
+    boost::system::error_code error;
+
+    size_t bytes_transferred = 0;
+    std::string commands[] = {"light", "security"};
+    while (isRunning) {
+        std::cout << "Choose an option: ";
+        std::cin >> s;
+
+        switch (s) {
+            case 1:
+                std::cout << "1";
+                break;
+            case 2:
+                handle_command(socket, commands[0]);
+                break;
+            default:
+                std::cout << "Invalid option.\n";
+                break;
+        }
+    }
+
+    return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,56 @@
+#include <boost/asio.hpp>
 #include <iostream>
+#include <sqlite3.h>
+
+using boost::asio::ip::tcp;
+
+void handle_connection(tcp::socket& socket) {
+    std::array<char, 256>     buffer;
+    boost::system::error_code error;
+
+    // Keep reading from the socket until the client closes the connection
+    while (true) {
+        size_t bytes_transferred = socket.read_some(boost::asio::buffer(buffer), error);
+
+        if (error) {
+            if (error == boost::asio::error::eof) {
+                break;
+            } else {
+                std::cerr << "Error receiving request: " << error.message() << std::endl;
+                return;
+            }
+        }
+
+        std::string message(buffer.data(), bytes_transferred);
+        std::cout << "Received message: " << message << std::endl;
+
+        std::string response;
+        if (message == "security") {
+            response = "Hi there!";
+        } else if (message == "What is your name?") {
+            response = "My name is Server.";
+        } else if (message == "What time is it?") {
+            time_t now = std::time(nullptr);
+            response   = std::ctime(&now);
+        } else {
+            response = "Unknown command.";
+        }
+
+        boost::asio::write(socket, boost::asio::buffer(response));
+    }
+}
 
 int main() {
-  std::cout << "Hello world!";
-  int i{};
-  return 0;
+    boost::asio::io_service io_service;
+
+    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 1234));
+
+    while (true) {
+        tcp::socket socket(io_service);
+        acceptor.accept(socket);
+
+        handle_connection(socket);
+    }
+
+    return 0;
 }
